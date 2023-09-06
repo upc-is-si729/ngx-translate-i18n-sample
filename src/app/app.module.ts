70291bcf338e4d3adee4d0e3df009ef795838790
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-06 13:50:02 -0500

    refactor: reformatted app component code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import { HomeComponent } from './public/pages/home/home.component';
-import { TranslationSampleComponent } from './public/components/translation-sample/translation-sample.component';
-import { LanguageSelectionComponent } from './public/components/language-selection/language-selection.component';
+import {HomeComponent} from './public/pages/home/home.component';
+import {TranslationSampleComponent} from './public/components/translation-sample/translation-sample.component';
+import {LanguageSelectionComponent} from './public/components/language-selection/language-selection.component';
 
 
 export function createTranslateLoader(http: HttpClient) {
@@ -19,29 +18,14 @@ export function createTranslateLoader(http: HttpClient) {
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    TranslationSampleComponent,
-    LanguageSelectionComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatButtonToggleModule,
-  ],
+  declarations: [AppComponent, HomeComponent, TranslationSampleComponent, LanguageSelectionComponent],
+  imports: [BrowserModule, HttpClientModule, TranslateModule.forRoot({
+    defaultLanguage: 'en', loader: {
+      provide: TranslateLoader, useFactory: (createTranslateLoader), deps: [HttpClient]
+    }
+  }), BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatButtonToggleModule,],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
